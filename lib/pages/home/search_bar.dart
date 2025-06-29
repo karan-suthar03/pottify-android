f22767f6926a339174c3f4187906fb119195b1be
karan suthar <karan.suthar.lakha@gmail.com>
2025-06-30 04:00:52 +0530

    Refactor: Improve audio player state management

--- lib/pages/home/search_bar.dart
+++ lib/pages/home/search_bar.dart
@@ -86,7 +86,6 @@ class _SearchBarState extends State<SearchBar> {
           ),
         ),
         textInputAction: TextInputAction.search,
-        onEditingComplete: _onSearch,
       ),
     );
   }
